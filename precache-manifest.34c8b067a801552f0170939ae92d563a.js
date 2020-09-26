7cf94db115c417c1ff916d1575e068d8e8338c57
z-igor <z-igor@nomail.com>
2020-09-26 13:16:09 +0300

    deploy Test-Messenger

---
+++ precache-manifest.34c8b067a801552f0170939ae92d563a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96389a7fb6dea3f4f4f4",
+    "url": "/vue_test_messange/css/app.74f0b96c.css"
+  },
+  {
+    "revision": "1aeea6c0b2f075b8ef09e6cda118d366",
+    "url": "/vue_test_messange/index.html"
+  },
+  {
+    "revision": "96389a7fb6dea3f4f4f4",
+    "url": "/vue_test_messange/js/app.caf90c8f.js"
+  },
+  {
+    "revision": "2e944f87d821d6655713",
+    "url": "/vue_test_messange/js/chunk-fff5b15e.d43a3b22.js"
+  },
+  {
+    "revision": "eb1747aae806bc11880b",
+    "url": "/vue_test_messange/js/chunk-vendors.f2228134.js"
+  },
+  {
+    "revision": "46bf1a5bcbabd1643dec61d9e824346a",
+    "url": "/vue_test_messange/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue_test_messange/robots.txt"
+  }
+]);
